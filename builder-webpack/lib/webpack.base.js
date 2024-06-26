e5438e449929b473e0dd420842adfbef13a62414
ao <1328497946@qq.com>
2024-06-26 10:36:17 +0800

    eslint配置

--- builder-webpack/lib/webpack.base.js
+++ builder-webpack/lib/webpack.base.js
@@ -1,121 +1,126 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const glob = require('glob');
 
 const setMPA = () => {
-	const entry = {};
-	const HtmlWebpackPlugins = [];
+  const entry = {};
+  const HtmlWebpackPlugins = [];
 
-	const entryFile = glob.sync(path.join(__dirname, "./src/*/index.js"));
+  const entryFile = glob.sync(path.join(__dirname, './src/*/index.js'));
 
-	entryFile.map((v) => {
-		const match = v.match(/src\/(.*)\/index.js/);
-		const pageName = match && match[1];
-		entry[pageName] = v;
-		HtmlWebpackPlugins.push(
-			new HtmlWebpackPlugin({
-				template: path.join(__dirname, `src/${pageName}/${pageName}.html`),
-				filename: `${pageName}.html`,
-				chunks: [pageName],
-				inject: true,
-				minify: {
-					html5: true,
-					collapseWhitespace: true,
-					preserveLineBreaks: false,
-					minifyCSS: true,
-					minifyJS: true,
-					removeComments: false,
-				},
-			})
-		);
-	});
+  entryFile.forEach((v) => {
+    const match = v.match(/src\/(.*)\/index.js/);
+    const pageName = match && match[1];
+    entry[pageName] = v;
+    HtmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `src/${pageName}/${pageName}.html`),
+        filename: `${pageName}.html`,
+        chunks: [pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false,
+        },
+      }),
+    );
+  });
 
-	return {
-		entry,
-		HtmlWebpackPlugins,
-	};
+  return {
+    entry,
+    HtmlWebpackPlugins,
+  };
 };
 
 const { entry, HtmlWebpackPlugins } = setMPA();
 
 module.exports = {
-	entry,
-	module: {
-		rules: [
-			{
-				test: /.js$/,
-				use: "babel-loader",
-			},
-			{
-				test: /.css$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader"],
-			},
-			{
-				test: /.less$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					"css-loader",
-					"less-loader",
-					{
-						loader: "postcss-loader",
-						options: {
-							plugins: () => {
-								require("autoprefixer")({
-									browsers: ["last 2 version", ">1%", "ios 4"],
-								});
-							},
-						},
-					},
-					{
-						loader: "px2rem-loader",
-						options: {
-							remUnit: 75,
-							remPrecession: 8,
-						},
-					},
-				],
-			},
-			{
-				test: /.(png|jpg|gif|jpeg)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							name: "[name]_[hash:8].[ext]",
-						},
-					},
-				],
-			},
-			{
-				test: /.(woff|woff2|eot|ttf|otf)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							name: "[name].[hash:8].[ext]",
-						},
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "[name]_[contenthash:8].css",
-		}),
-		new CleanWebpackPlugin(),
-		new FriendlyErrorsWebpackPlugin(),
-		function () {
-			this.hooks.done.tap("done", (stats) => {
-				if (
-					stats.compilation.errors &&
-					stats.compilation.errors.length &&
-					process.argv.indexOf("--watch" == -1)
-				) {
-					console.log("build error");
-					process.exit(1);
-				}
-			});
-		},
-	].concat(HtmlWebpackPlugins),
+  entry,
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => {
+                autoprefixer({
+                  browsers: ['last 2 version', '>1%', 'ios 4'],
+                });
+              },
+            },
+          },
+          {
+            loader: 'px2rem-loader',
+            options: {
+              remUnit: 75,
+              remPrecession: 8,
+            },
+          },
+        ],
+      },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css',
+    }),
+    new CleanWebpackPlugin(),
+    new FriendlyErrorsWebpackPlugin(),
+    function errorsPlugin() {
+      this.hooks.done.tap('done', (stats) => {
+        if (
+          stats.compilation.errors
+          && stats.compilation.errors.length
+          && process.argv.indexOf('--watch' === -1)
+        ) {
+          // eslint-disable-next-line no-console
+          console.log('build error');
+          process.exit(1);
+        }
+      });
+    },
+  ].concat(HtmlWebpackPlugins),
 };
--- builder-webpack/lib/webpack.prod.js
+++ builder-webpack/lib/webpack.prod.js
@@ -1,44 +1,45 @@
-const merge = require("webpack-merge");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const baseConfig = require("./webpack.base");
-const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
+const merge = require('webpack-merge');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+const cssnano = require('cssnano');
+const baseConfig = require('./webpack.base');
 
 const prodConfig = {
-	mode: "production",
-	plugins: [
-		new OptimizeCssAssetsPlugin({
-			assetNameRegExp: /\.css$/g,
-			cssProcessor: require("cssnano"),
-		}),
-		new HtmlWebpackExternalsPlugin({
-			externals: [
-				{
-					module: "react",
-					entry:
-						"https://cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min.js",
-					global: "React",
-				},
-				{
-					module: "react-dom",
-					entry:
-						"https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min.js",
-					global: "ReactDOM",
-				},
-			],
-		}),
-	],
-	optimization: {
-		splitChunks: {
-			minSize: 0,
-			cacheGroups: {
-				commons: {
-					name: "commons",
-					chunks: "all",
-					minChunks: 2,
-				},
-			},
-		},
-	},
+  mode: 'production',
+  plugins: [
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [
+        {
+          module: 'react',
+          entry:
+            'https://cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min.js',
+          global: 'React',
+        },
+        {
+          module: 'react-dom',
+          entry:
+            'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min.js',
+          global: 'ReactDOM',
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      minSize: 0,
+      cacheGroups: {
+        commons: {
+          name: 'commons',
+          chunks: 'all',
+          minChunks: 2,
+        },
+      },
+    },
+  },
 };
 
 module.exports = merge(baseConfig, prodConfig);
