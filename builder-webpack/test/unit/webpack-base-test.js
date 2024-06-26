9b2880a1a50263ce91a708a571c4196d931a5224
ao <1328497946@qq.com>
2024-06-26 17:59:01 +0800

    change

--- builder-webpack/test/unit/webpack-base-test.js
+++ builder-webpack/test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
   // eslint-disable-next-line no-console
   console.log(baseConfig);
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, 'C:/Users/ao/Desktop/webpack-test/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, 'C:/Users/ao/Desktop/webpack-test/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('/webpack-test/builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('/webpack-test/builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
